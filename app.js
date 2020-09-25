e36f356362c02e9fd873361ce33210be132bb60b
Shaun Gibb <sgibb1@mail.valenciacollege.edu>
2020-09-25 13:59:52 -0400

    added code for computer choice

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ userChoice = prompt("Pick either rock, paper or sissors.");
 
 computerChoice = Math.floor(Math.random() * 2);
 console.log(computerChoice);
+
+if (computerChoice == 0) {
+  computerChoice = "Rock"
+}
+else if (computerChoice == 1) {
+  computerChoice = "Paper"
+}
+else if (computerChoice == 2) {
+  computerChoice = "Sissors"
+} console.log("Computer: " + computerChoice);
