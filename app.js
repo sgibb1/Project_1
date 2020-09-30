9011e0d9851935b5af68f66e12d546620b056416
jahairabarrett <70175722+jahairabarrett@users.noreply.github.com>
2020-09-30 00:20:47 -0400

    Updated to compare users choice to computer.

--- app.js
+++ app.js
@@ -59,3 +59,22 @@ if (userChoice === "Paper") {
     }
   }
 }
+//////// Comparing Users Choice to Computer
+
+function results() {
+  if (computerChoice === userChoice) {
+    return result = 'You tied!'
+  } else if (computerChoice === 'rock' && userChoice === 'paper') { 
+    return result = 'You win!'
+  } else if (computerChoice === 'rock' && userChoice === 'scissors') {
+    return result = 'You lose'
+  } else if (computerChoice === 'paper' && userChoice === 'rock') {
+   return result = 'You lose'
+  } else if (computerChoice === 'paper' && userChoice === 'scissors') {
+   return result = 'You win!'
+  } else if (computerChoice === 'scissor' && userChoice === 'rock') {
+    return result = 'You win!'
+  } else if (computerChoice === 'scissor' && userChoice === 'paper') {
+    return result = 'You lose'
+  }
+}
